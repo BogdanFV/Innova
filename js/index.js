e386fe3e9f84155c8b04b886b114813504c919a2
uncle_bogdan <fbogdan13@gmail.com>
2021-11-25 13:30:08 +0300

    main page ready

---
+++ js/index.js
@@ -0,0 +1,12 @@
+/*let menuParent = document.querySelector('.header-menu');
+let menuOption = menuParent.querySelectorAll('.menu-option');
+menuParent.addEventListener('click', (event) => {
+    let target = event.target;
+    if(target.classList.contains('menu-option')) {
+      for(let i = 0; i < menuOption.length; i++) {
+        menuOption[i].classList.remove('active');
+      }
+      target.classList.add('active');
+    }
+});
+*/
